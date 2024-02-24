47d9e13fc9f514cea004de0cb8fb639ba578d3c0
YildirayKS <yildiraykarasubasi13@gmail.com>
2024-02-24 15:29:55 +0300

    Files Created & Sent

---
+++ 2427_Number_of_Common_Factors/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public int CommonFactors(int a, int b)
+    {
+        List<int> commonFactors = new List<int>();
+
+        for (int i = 1; i <= a; i++)
+        {
+            if (a % i == 0 && b % i == 0)
+            {
+                commonFactors.Add(i);
+            }
+        }
+        return commonFactors.Count();
+    }
+}
---
+++ 771_Jewels_and_Stones/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public int NumJewelsInStones(string jewels, string stones)
+    {
+        char[] lettersOfJewels = jewels.ToCharArray();
+
+        char[] lettersOfStones = stones.ToCharArray();
+
+        int counter = 0;
+
+        for (int i = 0; i < lettersOfJewels.Length; i++)
+        {
+            for (int j = 0; j < lettersOfStones.Length; j++)
+            {
+                if (lettersOfJewels[i] == lettersOfStones[j])
+                {
+                    counter++;
+                }
+            }
+        }
+
+        return counter;
+    }
+}
