e52b7adccf082fb9b157e7d3f2f20da4205539d9
YildirayKS <yildiraykarasubasi13@gmail.com>
2024-07-31 16:42:23 +0300

    new solution added

---
+++ 1672_Richest_Customer_Wealth/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{    public int MaximumWealth(int[][] accounts)
+    {
+        int maxSum = 0;
+        int temp = 0;
+
+        for (int i = 0; i < accounts.Length; i++)
+        {
+            for (int j = 0; j < accounts[i].Length; j++)
+            {
+                temp += accounts[i][j];
+            }
+
+            if (maxSum < temp)
+            {
+                maxSum = temp;
+            }
+
+            temp = 0;
+        }
+        return maxSum;
+    }
+}
